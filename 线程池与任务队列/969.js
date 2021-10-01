5c2d8b0de2d16b31ef37bc3c7e6ee2bfcfa18cc9
zxlfly <zxlfly@foxmail.com>
2021-10-01 18:21:47 +0800

    update duilie

--- 线程池与任务队列/969.js
+++ 线程池与任务队列/969.js
@@ -2,12 +2,34 @@
  * @param {number[]} arr
  * @return {number[]}
  */
- var pancakeSort = function(arr) {
-    if(arr.length<=1||arr==arr.reverse()){return []}
-    let ind = {}
-    let res= []
-    for(let i =0;i>arr.length;i++){
-        ind[arr[i]]=i
+ function getMaxIndex(a){
+    let max = 0
+    for(let i = 0 ;i<a.length;i++){
+        if(a[i]>a[max]){
+            max=i
+        }
     }
-    ind.sort((a,b)=>(a-b))
+    return max
+}
+function reverse(a,k){
+    if(k<1){return}
+    let i =0
+    while(i<k){
+        [a[i],a[k]]=[a[k],a[i]]
+        i++
+        k--
+    }
+}
+var pancakeSort = function(arr) {
+    let ans = []
+    let max
+    while (arr.length>1) {
+        max = getMaxIndex(arr)
+        max>0&&(ans.push(max+1))
+        reverse(arr,max)
+        reverse(arr,arr.length-1)
+        ans.push(arr.length)
+        arr.pop()
+    }
+    return ans
 };
